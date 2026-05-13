3532a564751768cadd5c3b1bb9614bd25c007d28
Ashitha Jacob <ashithajacob@intellicar.in>
2026-05-13 15:08:33 +0530

    Intial commit
    Datas are displayed
    page sliding is enabled

---
+++ inverter_display/Debug/ra/board/ra6m4_ek/board_init.d
@@ -0,0 +1,87 @@
+ra/board/ra6m4_ek/board_init.o: ../ra/board/ra6m4_ek/board_init.c \
+ C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra/fsp/inc/api/bsp_api.h \
+ C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra/fsp/inc/fsp_common_api.h \
+ C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra/fsp/inc/fsp_version.h \
+ C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra_cfg/fsp_cfg/bsp/bsp_cfg.h \
+ C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra_gen/bsp_clock_cfg.h \
+ C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra_cfg/fsp_cfg/bsp/bsp_mcu_family_cfg.h \
+ C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_pn_cfg.h \
+ C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_cfg.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\ra6m4\bsp_mcu_info.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\ra6m4\bsp_elc.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\ra6m4\bsp_feature.h \
+ C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra_cfg/fsp_cfg/bsp/board_cfg.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\board\ra6m4_ek\board.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\board\ra6m4_ek\board_init.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\board\ra6m4_ek\board_leds.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\board\ra6m4_ek\board_ethernet_phy.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_arm_exceptions.h \
+ C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra_gen/vector_data.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\cmsis\device\renesas\include\renesas.h \
+ C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h \
+ C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_gcc.h \
+ C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra/arm/CMSIS_5/CMSIS/Core/Include/core_cm33.h \
+ C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_version.h \
+ C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h \
+ C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra/arm/CMSIS_5/CMSIS/Core/Include/mpu_armv8.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\cmsis\device\renesas\include\base_addresses.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_common.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\inc\fsp_common_api.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_register_protection.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_irq.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_io.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_group_irq.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_clocks.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_module_stop.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_security.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\inc\fsp_features.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\inc\fsp_common_api.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_delay.h \
+ c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_mcu_api.h
+C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra/fsp/inc/api/bsp_api.h:
+C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra/fsp/inc/fsp_common_api.h:
+C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra/fsp/inc/fsp_version.h:
+C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra_cfg/fsp_cfg/bsp/bsp_cfg.h:
+C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra_gen/bsp_clock_cfg.h:
+C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra_cfg/fsp_cfg/bsp/bsp_mcu_family_cfg.h:
+C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_pn_cfg.h:
+C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_cfg.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\ra6m4\bsp_mcu_info.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\ra6m4\bsp_elc.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\ra6m4\bsp_feature.h:
+C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra_cfg/fsp_cfg/bsp/board_cfg.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\board\ra6m4_ek\board.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\board\ra6m4_ek\board_init.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\board\ra6m4_ek\board_leds.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\board\ra6m4_ek\board_ethernet_phy.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_arm_exceptions.h:
+C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra_gen/vector_data.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\cmsis\device\renesas\include\renesas.h:
+C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h:
+C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_gcc.h:
+C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra/arm/CMSIS_5/CMSIS/Core/Include/core_cm33.h:
+C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_version.h:
+C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h:
+C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra/arm/CMSIS_5/CMSIS/Core/Include/mpu_armv8.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\cmsis\device\renesas\include\base_addresses.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_common.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\inc\fsp_common_api.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_register_protection.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_irq.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_io.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_group_irq.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_clocks.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_module_stop.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_security.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\inc\fsp_features.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\inc\fsp_common_api.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_delay.h:
+c:\users\ashithajacob\documents\github\inverter_dwin_display\inverter_display\ra\fsp\src\bsp\mcu\all\bsp_mcu_api.h:
---
+++ inverter_display/Debug/src/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/hal_entry.c 
+
+C_DEPS += \
+./src/hal_entry.d 
+
+OBJS += \
+./src/hal_entry.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	$(file > $@.in,-mcpu=cortex-m33 -mthumb -mfloat-abi=hard -mfpu=fpv5-sp-d16 -O2 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wunused -Wuninitialized -Wall -Wextra -Wmissing-declarations -Wconversion -Wpointer-arith -Wshadow -Wlogical-op -Waggregate-return -Wfloat-equal  -g -D_RA_CORE=CM33 -D_RENESAS_RA_ -I"C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/src" -I"C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra/fsp/inc" -I"C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra/fsp/inc/api" -I"C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra/fsp/inc/instances" -I"C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra/arm/CMSIS_5/CMSIS/Core/Include" -I"C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra_gen" -I"C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra_cfg/fsp_cfg/bsp" -I"C:/Users/ashithajacob/Documents/GitHub/inverter_DWIN_display/inverter_display/ra_cfg/fsp_cfg" -std=c99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" -x c "$<")
+	@echo Building file: $< && arm-none-eabi-gcc @"$@.in"
+
